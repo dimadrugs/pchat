ea3a8407c7138b8f26ca426eb1701a93b68bb36e
dimadrugs <162021555+dimadrugs@users.noreply.github.com>
2026-03-15 20:48:11 +0500

    Create storage-config.js

---
+++ js/storage-config.js
@@ -0,0 +1,8 @@
+// ЭТОТ ФАЙЛ НЕ ЗАГРУЖАЕТСЯ НА GITHUB
+const STORAGE_CONFIG = {
+    keyId: 'YCAJEDgeAmsGuBZpbqidLhzYq',
+    secret: 'YCOcMyC96LccCohl12WYDPRyiWijbNwpFS9vbEsO',
+    bucket: 'pchat-files',
+    endpoint: 'https://storage.yandexcloud.net',
+    region: 'ru-central1'
+};
